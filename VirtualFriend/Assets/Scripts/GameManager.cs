d6cf199386bdd0b66874dd4d441cde6a1e921815
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2020-06-09 21:14:56 +0100

    Import GameManager

--- VirtualFriend/Assets/Scripts/GameManager.cs
+++ VirtualFriend/Assets/Scripts/GameManager.cs
@@ -1,18 +1,96 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject happinessText;
+    public GameObject hungerText;
+    public GameObject cleanlinessText;
+    public GameObject energyText;
+
+    public GameObject namePanel;
+    public GameObject nameInput;
+    public GameObject nameText;
+
+    public GameObject friend;
+    public GameObject friendPanel;
+    public GameObject[] friendList;
+
+    private void Start()
     {
-        
+        /*if (!PlayerPrefs.HasKey("looks"))
+            PlayerPrefs.SetInt("looks", 0);
+        CreateFriend(PlayerPrefs.GetInt("looks"));*/
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        happinessText.GetComponent<Text>().text =
+            "" + friend.GetComponent<VirtualFriend>().pHappiness;
+        hungerText.GetComponent<Text>().text =
+            "" + friend.GetComponent<VirtualFriend>().pHunger;
+        cleanlinessText.GetComponent<Text>().text =
+            "" + friend.GetComponent<VirtualFriend>().pCleanliness;
+        energyText.GetComponent<Text>().text =
+            "" + friend.GetComponent<VirtualFriend>().pEnergy;
+        nameText.GetComponent<Text>().text =
+            friend.GetComponent<VirtualFriend>().pName;
+    }
+
+    public void TriggerNamePanel(bool b)
+    {
+        namePanel.SetActive(!namePanel.activeInHierarchy);
+
+        if (b)
+        {
+            friend.GetComponent<VirtualFriend>().name =
+                nameInput.GetComponent<InputField>().text;
+            PlayerPrefs.SetString(
+                "Name", friend.GetComponent<VirtualFriend>().name);
+        }
+    }
+
+    public void ButtonBehavior(int i)
+    {
+        switch (i)
+        {
+            case (0):
+            default:
+                friendPanel.SetActive(!friendPanel.activeInHierarchy);
+                break;
+            case (1):
+
+                break;
+            case (2):
+
+                break;
+            case (3):
+
+                break;
+            case (4):
+                friend.GetComponent<VirtualFriend>().SaveRobot();
+                Application.Quit();
+                break;
+
+        }
+    }
+
+    public void CreateFriend(int i)
+    {
+        if (friend)
+            Destroy(friend);
+        friend = Instantiate(
+            friendList[i], Vector3.zero, Quaternion.identity) as GameObject;
+
+        if (friendPanel.activeInHierarchy)
+            friendPanel.SetActive(false);
+
+        PlayerPrefs.SetInt("looks", i);
+    }
+
+    public void Toggle(GameObject g)
+    {
+        if (g.activeInHierarchy)
+            g.SetActive(false);
     }
 }
