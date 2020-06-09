a82295ff374c9aa772be42646c32ce29aa032d84
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2020-06-09 21:15:54 +0100

    Import MenuManager

--- VirtualFriend/Assets/Scripts/MenuManager.cs
+++ VirtualFriend/Assets/Scripts/MenuManager.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject flashText;
+
     void Start()
     {
-        
+        InvokeRepeating("flashTheText", 0f, 0.5f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonUp(0))
+            SceneManager.LoadScene("Game");
+    }
+
+    void flashTheText()
+    {
+        if (flashText.activeInHierarchy)
+            flashText.SetActive(false);
+        else
+            flashText.SetActive(true);
     }
 }
