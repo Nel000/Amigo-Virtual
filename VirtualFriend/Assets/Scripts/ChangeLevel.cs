abfbaf0a66cd285618cd7b390805ed6ac818d162
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2020-06-10 21:59:37 +0100

    End runner after seconds

---
+++ VirtualFriend/Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(loadSceneAfterDelay(25));
+    }
+
+    IEnumerator loadSceneAfterDelay(float waitBySecs)
+    {
+        yield return new WaitForSeconds(waitBySecs);
+        SceneManager.LoadScene("Main");
+    }
+}
